8fd9868913d0ec0eb0a612a38c9d014d16acdb51
Shreyas2545 <shreyashadawale25@gmail.com>
2025-11-03 11:00:10 +0530

    updated the test model with 3 convolutional layer

--- src/test_model.py
+++ src/test_model.py
@@ -5,22 +5,24 @@ from torch.utils.data import DataLoader
 import os
 
 # -----------------------------
-# 1️⃣ CNN MODEL (SAME AS TRAINING)
+# 1️⃣ CNN MODEL (same as training)
 # -----------------------------
 class SimpleCNN(nn.Module):
     def __init__(self, num_classes=3):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, 1, 1)
-        self.conv2 = nn.Conv2d(16, 32, 3, 1, 1)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 32 * 32, 128)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.fc1 = nn.Linear(64 * 16 * 16, 128)
         self.fc2 = nn.Linear(128, num_classes)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.25)
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
         x = x.view(x.size(0), -1)
         x = self.dropout(self.relu(self.fc1(x)))
         x = self.fc2(x)
