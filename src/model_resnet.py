188809539ee2da5945a4664370370e92f8012c31
Shreyas2545 <shreyashadawale25@gmail.com>
2025-12-11 11:20:32 +0530

    resnet learning

--- src/model_resnet.py
+++ src/model_resnet.py
@@ -5,6 +5,7 @@ def get_resnet18_model(num_classes):
     model = resnet18(weights=ResNet18_Weights.DEFAULT) # 18-layer CNN and already trained on 1,000 ImageNet classes
 
     # 🔒 Freeze only early layers (keep deeper ones trainable)
+    #Freeze the early layers (they know basic like edgae , etc ) and train only ther deeper layers only 
     for name, param in model.named_parameters():
         if "layer4" not in name and "fc" not in name:
             param.requires_grad = False
@@ -12,9 +13,8 @@ def get_resnet18_model(num_classes):
     # 🔄 Replace final fully connected layer
     model.fc = nn.Sequential(
         nn.Linear(model.fc.in_features, 256),
-        nn.ReLU(),
-        nn.Dropout(0.3),
-        nn.Linear(256, num_classes)
+        nn.ReLU(), # Non-linearity
+        nn.Dropout(0.3), # dropping out 30% of the neurons
+        nn.Linear(256, num_classes) 
     )
-
     return model
